7597455568860c8468210edf5e229100f5bab4e6
gherardovarando <gherardo.varando@gmail.com>
2017-01-16 10:12:19 +0100

    help page link to github fixed

--- app/src/extensions/helpPage/helpPage.js
+++ app/src/extensions/helpPage/helpPage.js
@@ -109,7 +109,7 @@ class helpPage extends GuiExtension {
             title: 'Source code',
             icon: 'icon icon-github-circled',
             onclick: () => {
-                this.loadurl('https://github.com/gherardovarando/prova/');
+                this.loadurl('https://github.com/gherardovarando/Atlas/');
             }
         });
 
@@ -118,7 +118,7 @@ class helpPage extends GuiExtension {
             title: 'Submit issue',
             icon: 'icon icon-github',
             onclick: () => {
-                this.loadurl('https://github.com/gherardovarando/prova/issues/new');
+                this.loadurl('https://github.com/gherardovarando/Atlas/issues/new');
             }
         });
 

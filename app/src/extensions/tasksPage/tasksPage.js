cdac1ff2d55ef8d38b16d09fa537321710313af3
mjuez <mjuezath@gmail.com>
2017-02-21 12:07:15 +0100

    Added finished tasks notification badge.

--- app/src/extensions/tasksPage/tasksPage.js
+++ app/src/extensions/tasksPage/tasksPage.js
@@ -46,25 +46,32 @@ class tasksPage extends GuiExtension {
             className: 'btn btn-default pull-right',
             icon: icon
         });
+
+        this.toggleButton = this.buttonsContainer.buttons[`${toggleButtonId}`];
+        this.notifBadge = document.createElement("I");
+        this.notifBadge.className = "fa fa-circle notif-off";
+        this.toggleButton.appendChild(this.notifBadge);
+
         this.addPane();
         this.element.appendChild(this.pane.element);
-        
+
         this.taskManagerChangeListener = (...args) => {
             if (args.length === 1) {
                 let taskId = args[0];
                 let task = taskManager.tasks[taskId].task;
                 let domElement = taskManager.tasks[taskId].domElement.element;
 
-                if(task.status === Task.Status.RUNNING || task.status === Task.Status.CREATED){
-                    if(!this.runningTasksContainer.contains(domElement)){
+                if (task.status === Task.Status.RUNNING || task.status === Task.Status.CREATED) {
+                    if (!this.runningTasksContainer.contains(domElement)) {
                         this.runningTasksContainer.insertBefore(domElement, this.runningTasksContainer.firstChild);
                     }
-                }else{
-                    if(this.runningTasksContainer.contains(domElement)){
+                } else {
+                    if (this.runningTasksContainer.contains(domElement)) {
                         this.runningTasksContainer.removeChild(domElement);
                     }
-                    if(!this.finishedTasksContainer.contains(domElement)){
+                    if (!this.finishedTasksContainer.contains(domElement)) {
                         this.finishedTasksContainer.insertBefore(domElement, this.finishedTasksContainer.firstChild);
+                        this._updateBadge();
                     }
                 }
             }
@@ -73,7 +80,7 @@ class tasksPage extends GuiExtension {
         this.taskRemovedListener = (...args) => {
             if (args.length === 1) {
                 let domElement = args[0].element;
-                if(this.finishedTasksContainer.contains(domElement)){
+                if (this.finishedTasksContainer.contains(domElement)) {
                     this.finishedTasksContainer.removeChild(domElement);
                 }
             }
@@ -88,6 +95,11 @@ class tasksPage extends GuiExtension {
         taskManager.removeListener("change", this.taskManagerChangeListener);
         taskManager.removeListener("task.removed", this.taskRemovedListener);
         super.deactivate();
+    }
+
+    show(){
+        super.show();
+        this._updateBadge();
     }
 
     addPane() {
@@ -118,6 +130,24 @@ class tasksPage extends GuiExtension {
         rightContainer.appendChild(this.finishedTasksContainer);
         this.pane.element.appendChild(leftContainer);
         this.pane.element.appendChild(rightContainer);
+    }
+
+    _isHidden() {
+        return this.element.style.display === 'none';
+    }
+
+    _updateBadge() {
+        if (this._isHidden()) {
+            if (this.notifBadge.classList.contains("notif-off")) {
+                this.notifBadge.classList.remove("notif-off");
+                this.notifBadge.classList.add("notif-on");
+            }
+        }else{
+            if (this.notifBadge.classList.contains("notif-on")) {
+                this.notifBadge.classList.remove("notif-on");
+                this.notifBadge.classList.add("notif-off");
+            }
+        }
     }
 }
 

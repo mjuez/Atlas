22c0293a1444e143eb2893ee873955be813027d6
Mario Juez <mjuezath@gmail.com>
2017-03-22 15:43:47 +0100

    added new functionalities.

--- app/src/extensions/mapPage/_modules/LayersWidget.js
+++ app/src/extensions/mapPage/_modules/LayersWidget.js
@@ -3,11 +3,15 @@
 const Util = require('Util');
 const ListGroup = require('ListGroup');
 const Grid = require('Grid');
+const {
+    Menu,
+    MenuItem
+} = require('electron').remote;
+const Input = require('Input');
 
 class LayersWidget {
 
-    constructor(mapManager) {
-        this.mapManager = mapManager;
+    constructor() {
         this.element = Util.div(null, 'layers-widget');
         this.content = Util.div(null, 'content');
         this.list = new ListGroup(this.content);
@@ -29,8 +33,8 @@ class LayersWidget {
         this.element.appendChild(topBar);
         this.element.appendChild(this.content);
         this.baseLayer = null;
-        this.baseLayers = [];
-        this.overlayLayers = [];
+        this.baseLayers = {};
+        this.overlayLayers = {};
     }
 
     setMapManager(mapManager) {
@@ -38,65 +42,236 @@ class LayersWidget {
         this.mapManager.on('clean', () => {
             this.list.clean();
             this.baseLayer = null;
-            this.baseLayers = [];
-            this.overlayLayers = [];
+            this.baseLayers = {};
+            this.overlayLayers = {};
         });
 
         this.mapManager.on('add:tileslayer', (e) => {
-            let details = Util.div('tools will be here');
-
             let configuration = e.configuration;
             let layer = e.layer;
 
-            let actions = document.createElement('i');
+            let tools = this.createToolbox(layer, true, false, false);          
 
             if (configuration.baseLayer) {
-                this.baseLayers.push(layer);
-                if (!this.baseLayer) {
-                    this.baseLayer = layer;
-                    actions.className = 'fa fa-eye fa-lg';
-                    this.mapManager._map.addLayer(this.baseLayer);
-                } else {
-                    actions.className = 'fa fa-eye-slash fa-lg';
-                }
+                this.addBaseLayer(layer);
             } else {
-                actions.className = 'fa fa-eye-slash fa-lg';
+                this.addOverlayLayer(layer);
             }
 
-            actions.onclick = (e) => {
-                e.stopPropagation();
-                this.mapManager._map.removeLayer(this.baseLayer);
-                if (actions.classList.contains('fa-eye-slash')) {
-                    actions.classList.remove('fa-eye-slash');
-                    actions.classList.add('fa-eye');
-                    this.baseLayer = layer;
-                    this.mapManager._map.addLayer(layer);
-                } else {
-                    actions.classList.remove('fa-eye');
-                    actions.classList.add('fa-eye-slash');
-                }
-            };
+            let customMenuItems = [];
 
-            this.list.addItem({
-                title: configuration.name,
-                subtitle: configuration.authors,
-                body: details,
-                actions: actions,
-                key: configuration.name,
-                toggle: {
-                    justOne: true,
-                    expand: true
-                },
-                onclick: {
-                    active: () => {
+            let calibrationSettingsMenuItem = new MenuItem({
+                label: 'Calibration settings',
+                click: () => {
 
-                    },
-
-                    deactive: () => {
-                    }
                 }
             });
+            customMenuItems.push(calibrationSettingsMenuItem);
+
+            let baseLayerMenuItem = new MenuItem({
+                label: 'Base layer',
+                type: 'checkbox',
+                checked: configuration.baseLayer,
+                click: () => {
+                    configuration.baseLayer = baseLayerMenuItem.checked;
+                    this.mapManager.removeLayer(layer);
+                    this.mapManager.addLayer(configuration);
+                }
+            });
+            customMenuItems.push(baseLayerMenuItem);
+
+            this._addToList(layer, customMenuItems, tools);
         });
+
+        this.mapManager.on('remove:layer', (e) => {
+            if (e.configuration.baseLayer) {
+                this.removeBaseLayer(e.configuration.id);
+            } else {
+                this.removeOverlayLayer(e.configuration.id);
+            }
+            this.list.removeItem(e.configuration.id);
+        });
+    }
+
+    addBaseLayer(layer) {
+        let btnVisibility = document.createElement('i');
+
+        if (!this.baseLayer) {
+            this.baseLayer = layer;
+            btnVisibility.className = 'fa fa-eye fa-lg';
+            this.mapManager._map.addLayer(this.baseLayer);
+        } else {
+            btnVisibility.className = 'fa fa-eye-slash fa-lg';
+        }
+
+        btnVisibility.onclick = (e) => {
+            e.stopPropagation();
+            this.mapManager._map.removeLayer(this.baseLayer);
+            if (btnVisibility.classList.contains('fa-eye-slash')) {
+                Object.keys(this.baseLayers).map((key) => {
+                    let tempBtn = this.baseLayers[key].btnVisibility;
+                    if (tempBtn.classList.contains('fa-eye')) {
+                        tempBtn.classList.remove('fa-eye');
+                        tempBtn.classList.add('fa-eye-slash');
+                    }
+                });
+                btnVisibility.classList.remove('fa-eye-slash');
+                btnVisibility.classList.add('fa-eye');
+                this.baseLayer = layer;
+                this.mapManager._map.addLayer(layer);
+            } else {
+                btnVisibility.classList.remove('fa-eye');
+                btnVisibility.classList.add('fa-eye-slash');
+            }
+        };
+
+        this.baseLayers[layer._configuration.id] = {
+            layer: layer,
+            btnVisibility: btnVisibility
+        }
+    }
+
+    /**
+     * Removes a base layer from the widget.
+     * @param {number} idLayer id of the layer to remove.
+     */
+    removeBaseLayer(idLayer) {
+        this._removeLayer(idLayer, this.baseLayers);
+    }
+
+    addOverlayLayer(layer) {
+        let btnVisibility = document.createElement('i');
+
+        btnVisibility.className = 'fa fa-eye-slash fa-lg';
+
+        btnVisibility.onclick = (e) => {
+            e.stopPropagation();
+            this.mapManager._map.removeLayer(layer);
+            if (btnVisibility.classList.contains('fa-eye-slash')) {
+                btnVisibility.classList.remove('fa-eye-slash');
+                btnVisibility.classList.add('fa-eye');
+                this.mapManager._map.addLayer(layer);
+            } else {
+                btnVisibility.classList.remove('fa-eye');
+                btnVisibility.classList.add('fa-eye-slash');
+            }
+        };
+
+        this.overlayLayers[layer._configuration.id] = {
+            layer: layer,
+            btnVisibility: btnVisibility
+        }
+    }
+
+    /**
+     * Removes an overlay layer from the widget.
+     * @param {number} idLayer id of the layer to remove. 
+     */
+    removeOverlayLayer(idLayer) {
+        this._removeLayer(idLayer, this.overlayLayers);
+    }
+
+    /**
+     * Removes a layer from a list of layers.
+     * @param {number} idLayer id of the layer to remove.
+     * @param {Object} layers Object that contains a list of layers.
+     */
+    _removeLayer(idLayer, layers) {
+        if (layers[idLayer]) {
+            let layer = layers[idLayer];
+            if (this.mapManager._map.hasLayer(layer)) {
+                this.mapManager._map.removeLayer(layer);
+            }
+            delete layers[idLayer];
+        }
+    }
+
+    _addToList(layer, customMenuItems, tools) {
+        let txtTitle = Input.input({
+            value: layer._configuration.name,
+            className: 'list-input',
+            readOnly: true,
+            onblur: () => {
+                txtTitle.readOnly = true;
+            }
+        });
+
+        let context = new Menu();
+        context.append(new MenuItem({
+            label: 'Rename',
+            click: () => {
+                txtTitle.readOnly = false;
+            }
+        }));
+
+        context.append(new MenuItem({
+            label: 'Delete',
+            click: () => {
+                this.mapManager.removeLayer(layer);
+            }
+        }));
+
+        customMenuItems.map((menuItem) => {
+            context.append(menuItem);
+        });
+
+        let btnVisibility;
+        if (layer._configuration.baseLayer) {
+            btnVisibility = this.baseLayers[layer._configuration.id].btnVisibility;
+        } else {
+            btnVisibility = this.overlayLayers[layer._configuration.id].btnVisibility;
+        }
+
+        this.list.addItem({
+            id: layer._configuration.id,
+            title: txtTitle,
+            subtitle: layer._configuration.authors,
+            body: tools,
+            contextMenu: context,
+            actions: btnVisibility,
+            key: layer._configuration.name,
+            toggle: {
+                justOne: true,
+                expand: true
+            }
+        });
+    }
+
+    createToolbox(layer, hasOpacityControl, hasColorControl, hasRadiusControl) {
+        let toolbox = Util.div(null, 'table-container toolbox');
+        let configuration = layer._configuration;
+
+        if (hasColorControl) {
+
+        }
+
+        if (hasRadiusControl) {
+
+        }
+
+        if (hasOpacityControl) {
+            let opacityCell = Util.div(null, 'cell');
+
+            let input = Input.input({
+                label: '',
+                className: 'form-control',
+                parent: opacityCell,
+                type: 'range',
+                max: 1,
+                min: 0,
+                step: 0.1,
+                value: configuration.opacity,
+                placeholder: 'opacity',
+                oninput: (inp) => {
+                    configuration.opacity = Number(inp.value);
+                    this.mapManager.getLayers('tilesLayer')[configuration.typeid].setOpacity(configuration.opacity);
+                }
+            });
+
+            toolbox.appendChild(opacityCell);
+        }
+
+        return toolbox;
     }
 
     reload() {
